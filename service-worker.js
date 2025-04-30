6c226ef43faa645ee709c3f8f2533bc1ba6e03ce
MjoelGV <mjoelgv@gmail.com>
2025-04-30 11:27:35 -0500

    Añadir responsive menu móvil, random verse diario y actualizar SW

---
+++ service-worker.js
@@ -0,0 +1,18 @@
+// Service Worker stub to enable registration and always fetch from network
+const SW_VERSION = 'v2';
+console.log('Service Worker version:', SW_VERSION);
+
+self.addEventListener('install', event => {
+    // Activate immediately
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+    // Take control of all clients immediately
+    event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    // Always fetch from network
+    event.respondWith(fetch(event.request));
+});
