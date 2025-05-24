1d37cf986153fe4ed1f2f26e22f7abcfd6112ecd
MJoelGV <mjoelgv@gmail.com>
2025-05-23 22:56:42 -0500

    audio

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,20 @@
-const CACHE_NAME = 'biblia-pdm-cache-v2';
+const CACHE_NAME = 'biblia-pdm-cache-v3';
 const urlsToCache = [
-  './',
-  './index.html',
-  './lectura.html',
-  './audio.html',
-  './books.html',
-  './topics.html',
-  './manifest.json',
-  './icons/pymicon.png'
+  '/',
+  '/index.html',
+  '/lectura.html',
+  '/audio.html',
+  '/bookmarks.html',
+  '/comments.html',
+  '/diccionario.html',
+  '/historias.html',
+  '/memorizados.html',
+  '/manifest.json',
+  '/icons/pymicon.png',
+  '/css/styles.css',
+  '/js/script.js',
+  'https://fonts.googleapis.com/css2?family=Great+Vibes&display=swap',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
 
 // Install: cache assets and activate immediately
